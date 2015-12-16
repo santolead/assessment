923b4574da0015c7c6593fe5c3d45b4a0726c8e1
santolead <santolead.com@gmail.com>
2015-12-16 07:27:10 +0700

    fix

--- assessment2/js/scr.js
+++ assessment2/js/scr.js
@@ -6,6 +6,16 @@
 -------------------------------------------------- */
 
 jQuery(document).ready(function($){
+	
+	$('button').on('click',function(e) {
+		if ($(this).hasClass('grid')) {
+			$('#container ul').removeClass('list').addClass('grid');
+		}
+		else if($(this).hasClass('list')) {
+			$('#container ul').removeClass('grid').addClass('list');
+		}
+	});
+	
 	$('#smoothup').on('click', function(){
 		$('html, body').animate({scrollTop:0}, 'fast');
 		return false;
